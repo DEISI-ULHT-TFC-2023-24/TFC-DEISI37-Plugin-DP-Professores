ebe50246dff17eb88f054166441763b41164845c
joao-marques-a22108693 <117601737+joao-marques-a22108693@users.noreply.github.com>
2023-10-02 00:49:06 +0100

    Move functions to companion object

--- src/main/kotlin/com/tfc/ulht/dpplugin/Submissions.kt
+++ src/main/kotlin/com/tfc/ulht/dpplugin/Submissions.kt
@@ -10,59 +10,64 @@ import java.io.InputStream
 import javax.swing.JOptionPane
 
 class SubmissionsAction : AnAction() {
-    private fun createFile(id: String, stream: InputStream): String? {
-        val path = System.getProperty("java.io.tmpdir") + File.separatorChar + "sub_$id.zip"
+    companion object {
+        private fun createFile(id: String, stream: InputStream): String? {
+            val path = System.getProperty("java.io.tmpdir") + File.separatorChar + "sub_$id.zip"
 
-        try {
-            val file = File(path)
+            try {
+                val file = File(path)
 
-            file.outputStream().write(stream.readAllBytes())
-        } catch (e: Exception) {
-            Logger.getInstance(SubmissionsAction::class.java).error("Couldn't create file, ${e.message}")
-            return null
-        }
-
-        return path
-    }
-
-    private fun unzipSubmission(path: String): String? {
-        val finalPath = path.removeSuffix(".zip")
-
-        try {
-            with (File(finalPath)) {
-                if (this.exists()) this.deleteRecursively()
+                file.outputStream().write(stream.readAllBytes())
+            } catch (e: Exception) {
+                Logger.getInstance(SubmissionsAction::class.java).error("Couldn't create file, ${e.message}")
+                return null
             }
 
-            ZipFile(path).extractAll(finalPath)
-
-            File(path).delete()
-        } catch (e: Exception) {
-            Logger.getInstance(SubmissionsAction::class.java).error("Couldn't extract file, ${e.message}")
-            return null
+            return path
         }
 
-        return finalPath
-    }
+        private fun unzipSubmission(path: String): String? {
+            val finalPath = path.removeSuffix(".zip")
 
-    override fun actionPerformed(e: AnActionEvent) {
-        val id = JOptionPane.showInputDialog("Please input the submission ID.")
+            try {
+                with (File(finalPath)) {
+                    if (this.exists()) this.deleteRecursively()
+                }
 
-        State.client.downloadSubmission(id) {
-            if (it == null) {
-                JOptionPane.showMessageDialog(
-                    null, "Error", "Couldn't download the submission", JOptionPane.ERROR_MESSAGE
-                )
-            } else {
-                val unzippedPath = createFile(id, it)?.let { path -> unzipSubmission(path) }
+                ZipFile(path).extractAll(finalPath)
 
-                if (unzippedPath != null) {
-                    ProjectManager.getInstance().loadAndOpenProject(unzippedPath)
-                } else {
+                File(path).delete()
+            } catch (e: Exception) {
+                Logger.getInstance(SubmissionsAction::class.java).error("Couldn't extract file, ${e.message}")
+                return null
+            }
+
+            return finalPath
+        }
+
+        fun openSubmission(id: String) {
+
+            State.client.downloadSubmission(id) {
+                if (it == null) {
                     JOptionPane.showMessageDialog(
-                        null, "Error", "Couldn't extract the submission", JOptionPane.ERROR_MESSAGE
+                        null, "Error", "Couldn't download the submission", JOptionPane.ERROR_MESSAGE
                     )
+                } else {
+                    val unzippedPath = createFile(id, it)?.let { path -> unzipSubmission(path) }
+
+                    if (unzippedPath != null) {
+                        ProjectManager.getInstance().loadAndOpenProject(unzippedPath)
+                    } else {
+                        JOptionPane.showMessageDialog(
+                            null, "Error", "Couldn't extract the submission", JOptionPane.ERROR_MESSAGE
+                        )
+                    }
                 }
             }
         }
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        openSubmission(JOptionPane.showInputDialog("Please input the submission ID."))
     }
 }
