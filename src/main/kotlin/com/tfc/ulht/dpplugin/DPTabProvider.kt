315339bc667f713cb4a4c3a6297ecfa94cdff3a4
joao-marques-a22108693 <117601737+joao-marques-a22108693@users.noreply.github.com>
2023-10-08 00:57:53 +0100

    Added a new tab for listing all submissions made by a specific group

--- src/main/kotlin/com/tfc/ulht/dpplugin/DPTabProvider.kt
+++ src/main/kotlin/com/tfc/ulht/dpplugin/DPTabProvider.kt
@@ -13,9 +13,11 @@ import com.intellij.testFramework.LightVirtualFile
 import com.intellij.util.ui.JBUI
 import com.tfc.ulht.dpplugin.dplib.Assignment
 import com.tfc.ulht.dpplugin.dplib.DPData
+import com.tfc.ulht.dpplugin.dplib.Submission
 import com.tfc.ulht.dpplugin.dplib.SubmissionsResponse
 import com.tfc.ulht.dpplugin.ui.AssignmentComponent
 import com.tfc.ulht.dpplugin.ui.GroupSubmissionsComponent
+import com.tfc.ulht.dpplugin.ui.SubmissionComponent
 import java.awt.Dimension
 import java.beans.PropertyChangeListener
 import javax.swing.*
@@ -92,6 +94,44 @@ fun groupSubmissionsTabProvider(data: List<DPData>) : DPPanel {
             this.addSubmissionDownloadClickListener { _ ->
                 SubmissionsAction.openSubmission(it.allSubmissions.first().id.toString())
             }
+            this.addAllSubmissionsClickListener {
+                root.tab.data = it.allSubmissions
+                root.tab.panel.removeAll()
+                root.tab.panel.add(root.tab.getTab(it.allSubmissions.first().javaClass.name))
+                root.tab.panel.revalidate()
+            }
+        })
+
+        submissionsPanel.add(JSeparator(SwingConstants.HORIZONTAL).apply {
+            maximumSize = Dimension(maximumSize.width, 2)
+        })
+    }
+
+    if (submissionsPanel.componentCount > 1) submissionsPanel.remove(submissionsPanel.componentCount - 1)
+
+    return root
+}
+@Suppress("UNCHECKED_CAST")
+fun submissionsTabProvider(data: List<DPData>) : DPPanel {
+    val root = DPPanel().apply {
+        this.layout = BoxLayout(this, BoxLayout.Y_AXIS)
+        this.border = JBUI.Borders.empty(0, 20)
+    }
+
+    val submissions = data as List<Submission>
+
+    root.add(JLabel("<html><h1>Submissions</h1></html").apply { alignmentX = 0.0f })
+    val submissionsPanel = JPanel().apply {
+        this.layout = BoxLayout(this, BoxLayout.Y_AXIS)
+        this.border = JBUI.Borders.empty(0, 10)
+    }
+    root.add(submissionsPanel)
+
+    submissions.forEach {
+        submissionsPanel.add(SubmissionComponent(it).apply {
+            this.addSubmissionDownloadClickListener {
+                SubmissionsAction.openSubmission(this.submission.id.toString())
+            }
         })
 
         submissionsPanel.add(JSeparator(SwingConstants.HORIZONTAL).apply {
@@ -106,7 +146,8 @@ fun groupSubmissionsTabProvider(data: List<DPData>) : DPPanel {
 
 val tabProviders = mapOf<String, (List<DPData>) -> DPPanel>(
     Pair(Assignment::class.java.name, ::assignmentTabProvider),
-    Pair(SubmissionsResponse::class.java.name, ::groupSubmissionsTabProvider)
+    Pair(SubmissionsResponse::class.java.name, ::groupSubmissionsTabProvider),
+    Pair(Submission::class.java.name, ::submissionsTabProvider)
 )
 
 class DPTab(val project: Project, var data: List<DPData>) : FileEditor {
--- src/main/kotlin/com/tfc/ulht/dpplugin/ui/Components.kt
+++ src/main/kotlin/com/tfc/ulht/dpplugin/ui/Components.kt
@@ -2,8 +2,9 @@ package com.tfc.ulht.dpplugin.ui
 
 import com.intellij.ui.JBColor
 import com.tfc.ulht.dpplugin.dplib.Assignment
+import com.tfc.ulht.dpplugin.dplib.Submission
 import com.tfc.ulht.dpplugin.dplib.SubmissionsResponse
-import java.awt.Cursor
+import java.awt.*
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
 import java.awt.font.TextAttribute
@@ -54,14 +55,122 @@ class AssignmentComponent(val assignment: Assignment) : JComponent() {
 }
 
 class GroupSubmissionsComponent(submissions: SubmissionsResponse) : JComponent() {
-    val idLabel: JLabel
-    val submissionDownloadLabel: JLabel
+    private val idLabel: JLabel
+    private val allSubmissions: NumberBox
+    private val submissionDownloadLabel: JLabel
 
     init {
         this.layout = BoxLayout(this, BoxLayout.X_AXIS)
         this.alignmentX = 0.0f
 
-        idLabel = JLabel("<html><h3>${submissions.projectGroup.authors.joinToString { a -> "student" + a.id }}</h3></html>")
+        // TODO: Create new label class that mimics the <h3> tag without expanding infinitely
+        idLabel = JLabel(submissions.projectGroup.authors.joinToString { a -> "student" + a.id })
+        this.add(idLabel)
+        allSubmissions = NumberBox(submissions.allSubmissions.size)
+        this.add(allSubmissions)
+
+        this.add(Box.createHorizontalGlue())
+
+        submissionDownloadLabel = JLabel("Download").apply {
+            this.foreground = JBColor.BLUE
+
+            val style = this.font.attributes.toMutableMap()
+            style[TextAttribute.UNDERLINE] = TextAttribute.UNDERLINE_ON
+            this.font = this.font.deriveFont(style)
+
+            this.cursor = Cursor(Cursor.HAND_CURSOR)
+        }
+
+        this.add(submissionDownloadLabel)
+    }
+
+    fun addSubmissionDownloadClickListener(listener: (MouseEvent?) -> Unit) = submissionDownloadLabel.addMouseListener(object : MouseListener {
+        override fun mouseClicked(e: MouseEvent?) = listener(e)
+
+        override fun mousePressed(e: MouseEvent?) {  }
+
+        override fun mouseReleased(e: MouseEvent?) {  }
+
+        override fun mouseEntered(e: MouseEvent?) {  }
+
+        override fun mouseExited(e: MouseEvent?) {  }
+    })
+
+    fun addAllSubmissionsClickListener(listener: () -> Unit) {
+        allSubmissions.clickListener = listener
+    }
+}
+
+class NumberBox(number: Int) : JComponent() {
+    private val numString = number.toString()
+    var mouseOver = false
+
+    var clickListener: (() -> Unit)? = null
+
+    init {
+        this.minimumSize = Dimension(5, 10)
+        cursor = Cursor(Cursor.HAND_CURSOR)
+        addMouseListener(object : MouseListener {
+            override fun mouseClicked(e: MouseEvent) {
+                clickListener?.invoke()
+            }
+            override fun mousePressed(e: MouseEvent) {}
+            override fun mouseReleased(e: MouseEvent) {}
+            override fun mouseEntered(e: MouseEvent) {
+                mouseOver = true
+                repaint()
+            }
+
+            override fun mouseExited(e: MouseEvent) {
+                mouseOver = false
+                repaint()
+            }
+        })
+    }
+
+    override fun getMaximumSize(): Dimension {
+        return Dimension(25, 25)
+    }
+
+    override fun getPreferredSize(): Dimension {
+        return maximumSize
+    }
+
+    public override fun paintComponent(g: Graphics) {
+        val g2 = g as Graphics2D
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        g2.color = JBColor.namedColor("Separator.separatorColor")
+
+        g2.drawRoundRect(0, 0, width - 1, height - 1, width / 4, height / 4)
+
+        if (mouseOver) {
+            g2.color = Color(JBColor.namedColor("Panel.background").rgb and (0xFF shl 4 * 8), true)
+            g2.fillRoundRect(1, 1, width - 2, height - 2, width / 4, height / 4)
+            g2.color = JBColor.BLACK
+        }
+
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+        val rec = g.getFont().getStringBounds(numString, g2.fontRenderContext)
+        g.drawChars(
+            numString.toCharArray(),
+            0,
+            numString.length,
+            (width / 2 - rec.width / 2).toInt() + 1,
+            (height / 2 - rec.y / 2).toInt()
+        )
+        super.paintComponent(g)
+    }
+}
+
+class SubmissionComponent(val submission: Submission) : JComponent() {
+    private val idLabel: JLabel
+    private val submissionDownloadLabel: JLabel
+
+    init {
+        this.layout = BoxLayout(this, BoxLayout.X_AXIS)
+        this.alignmentX = 0.0f
+
+        idLabel = JLabel("<html><h3>${submission.id}: ${submission.submissionDate}</h3></html>")
         this.add(idLabel)
 
         this.add(Box.createHorizontalGlue())
