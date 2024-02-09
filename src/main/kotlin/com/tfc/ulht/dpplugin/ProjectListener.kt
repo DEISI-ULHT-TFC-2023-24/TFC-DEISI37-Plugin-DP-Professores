0401f36a98e35163fa6ef54e51f1a192e52d5e38
joao-marques-a22108693 <117601737+joao-marques-a22108693@users.noreply.github.com>
2024-02-09 00:03:40 +0000

    Move logic to an invokeLater call

--- src/main/kotlin/com/tfc/ulht/dpplugin/ProjectListener.kt
+++ src/main/kotlin/com/tfc/ulht/dpplugin/ProjectListener.kt
@@ -2,19 +2,22 @@ package com.tfc.ulht.dpplugin
 
 import com.intellij.credentialStore.CredentialAttributes
 import com.intellij.ide.passwordSafe.PasswordSafe
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 import com.tfc.ulht.dpplugin.dplib.BASE_URL
 
 class ProjectListener : ProjectManagerListener {
     override fun projectOpened(project: Project) {
-        val credentials = PasswordSafe.instance.get(CredentialAttributes("DP", "dp"))
+        ApplicationManager.getApplication().invokeLater {
+            val credentials = PasswordSafe.instance.get(CredentialAttributes("DP", "dp"))
 
-        credentials?.userName?.let {
-            credentials.password?.let {
-                BASE_URL = credentials.password.toString()
+            credentials?.userName?.let {
+                credentials.password?.let {
+                    BASE_URL = credentials.password.toString()
+                }
+                State.client.login(it, null)
             }
-            State.client.login(it, null)
         }
     }
 }
