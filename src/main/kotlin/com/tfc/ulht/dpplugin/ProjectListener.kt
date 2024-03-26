a7a80331cdffd1797b91ca4a4901b8c98f86b03d
joao-marques-a22108693 <117601737+joao-marques-a22108693@users.noreply.github.com>
2024-03-26 14:45:22 +0000

    Potential fix for empty projects on load

--- src/main/kotlin/com/tfc/ulht/dpplugin/ProjectListener.kt
+++ src/main/kotlin/com/tfc/ulht/dpplugin/ProjectListener.kt
@@ -2,7 +2,11 @@ package com.tfc.ulht.dpplugin
 
 import com.intellij.credentialStore.CredentialAttributes
 import com.intellij.ide.passwordSafe.PasswordSafe
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.ex.ActionUtil
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 import com.tfc.ulht.dpplugin.dplib.BASE_URL
@@ -17,6 +21,19 @@ class ProjectListener : ProjectManagerListener {
                     BASE_URL = credentials.password.toString()
                 }
                 State.client.login(it, null)
+            }
+        }
+
+        if (project.name.startsWith("sub_")) {
+            ActionUtil.getAction("Synchronize")?.let {
+                ActionUtil.invokeAction(
+                    it,
+                    DataContext.EMPTY_CONTEXT,
+                    ActionPlaces.UNKNOWN,
+                    null
+                ) {
+                    Logger.getInstance(ProjectListener::class.java).debug("Action!")
+                }
             }
         }
     }
