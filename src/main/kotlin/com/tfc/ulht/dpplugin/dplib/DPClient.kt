4cc0e0771502ba94acd5ea18eb0d9e2107987ee9
joao-marques-a22108693 <117601737+joao-marques-a22108693@users.noreply.github.com>
2024-06-11 12:01:02 +0100

    Possible auth fix

--- src/main/kotlin/com/tfc/ulht/dpplugin/dplib/DPClient.kt
+++ src/main/kotlin/com/tfc/ulht/dpplugin/dplib/DPClient.kt
@@ -7,30 +7,54 @@ import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
 import java.io.InputStream
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class DPClient {
-    /* private val authenticator = object : Authenticator {
-        var username: String? = null
-        var token: String? = null
-
-        override fun authenticate(route: Route?, response: Response): Request? {
-            if (username == null || token == null || response.request.header("Authorization") != null) {
-                return null
-            }
-
-            return response.request.newBuilder()
-                .header("Authorization", Credentials.basic(username!!, token!!))
-                .build()
-        }
-    }*/
 
     companion object {
+        private val authenticator = object : Authenticator {
+            var username: String? = null
+            var token: String? = null
+
+            override fun authenticate(route: Route?, response: Response): Request? {
+                if (username == null || token == null || response.request.header("Authorization") != null) {
+                    return null
+                }
+
+                return response.request.newBuilder()
+                    .header("Authorization", Credentials.basic(username!!, token!!))
+                    .build()
+            }
+        }
+
         private val client = OkHttpClient.Builder()
             .connectTimeout(5, TimeUnit.SECONDS)
             .readTimeout(5, TimeUnit.SECONDS)
             .writeTimeout(5, TimeUnit.SECONDS)
+            .authenticator(authenticator)
             .build()
+
+        private fun OkHttpClient.Builder.ignoreAllSSLErrors(): OkHttpClient.Builder {
+            val naiveTrustManager = object : X509TrustManager {
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                override fun checkClientTrusted(certs: Array<X509Certificate>, authType: String) = Unit
+                override fun checkServerTrusted(certs: Array<X509Certificate>, authType: String) = Unit
+            }
+
+            val insecureSocketFactory = SSLContext.getInstance("TLSv1.2").apply {
+                val trustAllCerts = arrayOf<TrustManager>(naiveTrustManager)
+                init(null, trustAllCerts, SecureRandom())
+            }.socketFactory
+
+            sslSocketFactory(insecureSocketFactory, naiveTrustManager)
+            hostnameVerifier { _, _ -> true }
+            return this
+        }
     }
 
     private val json = Json { ignoreUnknownKeys = true }
